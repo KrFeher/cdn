0e15137267b3ac22ce9cb1992c1db298073b65a9
Krisztian <kris.feher@outlook.com>
2021-06-19 20:22:42 +0100

    Added form placeholder code

--- minipixel.js
+++ minipixel.js
@@ -1,1 +1,17 @@
-alert("test");
+let elements = document.getElementsByClassName("contact-field");
+
+let hidePlaceholder = function() {
+      this.placeholder='';
+  }
+
+let showPlaceholder = function(event, text) {
+    if (event.target.value === "") {
+        event.target.placeholder = text;
+    }
+};
+
+for (var i = 0; i < elements.length; i++) {
+    let placeholder = elements[i].placeholder;
+    elements[i].addEventListener('focusin', hidePlaceholder, false);
+    elements[i].addEventListener('blur', function(event) {showPlaceholder(event, placeholder)}, false)
+}
